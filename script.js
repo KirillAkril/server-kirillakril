83365d02bb4ac8ff63969a6f6c2c06d8fd7eeeaf
KirillAkril <demchenkokir07@gmail.com>
2026-05-09 16:49:55 +0300

    readme

--- script.js
+++
@@ -1,9 +0,0 @@
-function changeText() {
-    const text = document.getElementById("text");
-
-    if (text.innerText === "Hello, World!") {
-        text.innerText = "Привет, мир!";
-    } else {
-        text.innerText = "Hello, World!";
-    }
-}
